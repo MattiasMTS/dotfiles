63e8bc59f63d08ef3ca3a5937f9edf669134d155
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-06-11 09:56:12 +0200

    chore: lazygit improvements

--- home/programs/lazygit.nix
+++ home/programs/lazygit.nix
@@ -1,16 +1,28 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   enable = true;
 
   settings = {
     notARepository = "quit";
     gui = {
-      showBottomLine = false;
+      showBottomLine = true;
       border = "rounded";
       nerdFontsVersion = 3;
+      skipDiscardChangeWarning = false;
+      skipStashWarning = false;
+      mainPanelSplitMode = "flexible";
+      commitLength.show = true;
+      splitDiff = "auto";
+      commandLogSize = 6;
+      sidePanelWidth = 0.25;
     };
-    os.editPreset = "nvim";
+    os.editPreset = "nvim-remote";
     git.overrideGpg = true;
+    git.paging = {
+      colorArg = "always";
+      pager = "delta --dark --paging=never";
+      useConfig = false;
+    };
   };
 
 }
-
