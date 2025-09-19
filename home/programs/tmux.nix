87ef1a3b097ed21219e8bd0f4f7eff6e340ed9dc
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-09-19 07:20:45 +0200

    chore: update tmux look and feel

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -36,20 +36,60 @@ in
       plugin = catppuccin;
       extraConfig = ''
         set -g @catppuccin_flavor "macchiato"
-        set -g @catppuccin_window_status_style "rounded"
+        set -g @catppuccin_status_background "none"
+        set -g @catppuccin_window_status_style "none"
+        set -g @catppuccin_pane_status_enabled "off"
+        set -g @catppuccin_pane_border_status "off"
 
-        set -g status-right-length 100
+        # status left look and feel
         set -g status-left-length 100
         set -g status-left ""
-        set -g status-right "#{E:@catppuccin_status_application}"
-        set -ag status-right "#{E:@catppuccin_status_session}"
-        set -ag status-right "#{E:@catppuccin_status_uptime}"
+        set -ga status-left "#{?client_prefix,#{#[bg=#{@thm_red},fg=#{@thm_bg},bold]  #S },#{#[bg=#{@thm_bg},fg=#{@thm_green}]  #S }}"
+        set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]│"
+        set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_maroon}]  #{pane_current_command} "
+        set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]│"
+        set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_blue}]  #{=/-32/...:#{s|$USER|~|:#{b:pane_current_path}}} "
+        set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]#{?window_zoomed_flag,│,}"
+        set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_yellow}]#{?window_zoomed_flag,  zoom ,}"
+
+        # status right look and feel
+        set -g status-right-length 100
+        set -g status-right ""
+        set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}, none]│"
+        set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}, none]│"
+        set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_blue}] 󰭦 %Y-%m-%d 󰅐 %H:%M "
+
+
+
       '';
     }
   ];
   extraConfig = ''
+
+    # pane border look and feel
+    # setw -g pane-border-status top
+    # setw -g pane-border-format ""
+    setw -g pane-active-border-style "bg=#{@thm_bg},fg=#{@thm_overlay_0}"
+    setw -g pane-border-style "bg=#{@thm_bg},fg=#{@thm_surface_0}"
+    setw -g pane-border-lines single
+
+    # window look and feel
+    set -wg automatic-rename on
+    set -g automatic-rename-format "Window"
+
+    set -g window-status-format " #I#{?#{!=:#{window_name},Window},: #W,} "
+    set -g window-status-style "bg=#{@thm_bg},fg=#{@thm_rosewater}"
+    set -g window-status-last-style "bg=#{@thm_bg},fg=#{@thm_peach}"
+    set -g window-status-activity-style "bg=#{@thm_red},fg=#{@thm_bg}"
+    set -g window-status-bell-style "bg=#{@thm_red},fg=#{@thm_bg},bold"
+    set -gF window-status-separator "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}]│"
+
+    set -g window-status-current-format " #I#{?#{!=:#{window_name},Window},: #W,} "
+    set -g window-status-current-style "bg=#{@thm_peach},fg=#{@thm_bg},bold"
+
     # Undercurl
-    set -g default-terminal "${terminal}"
+    # set -g default-terminal "${terminal}"
+    set -g default-terminal "screen-256color"
     set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
     set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
 
