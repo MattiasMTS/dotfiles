cef4d7b9c45e04d55966b9d8c0772d24e6584ed7
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-09-19 08:55:23 +0200

    chore: update yamlls

--- .config/nvim/lsp/yamlls.lua
+++ .config/nvim/lsp/yamlls.lua
@@ -2,12 +2,31 @@
 return {
   cmd = { "yaml-language-server", "--stdio" },
   filetypes = { "yaml" },
+  before_init = function(_, new_config)
+    new_config.settings.yaml.schemas =
+      vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+  end,
   settings = {
     redhat = { telemetry = { enabled = false } },
     yaml = {
-      -- Using the schemastore plugin for schemas.
-      schemastore = { enable = false, url = "" },
-      schemas = require("schemastore").yaml.schemas(),
+      keyOrdering = false,
+      format = {
+        enable = true,
+      },
+      validate = true,
+      schemaStore = {
+        enable = false, -- use schemastore.nvim instead
+        url = "",
+      },
+    },
+  },
+  -- Enable folding range capability, as the server supports it
+  capabilities = {
+    textDocument = {
+      foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      },
     },
   },
 }
