3d72d756f1b1fa16e907a2fa76bcf6d689aa54d2
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-09-20 16:52:48 +0200

    chore: update blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -12,28 +12,6 @@ return {
     build = "cargo build --release",
     event = "InsertEnter",
     opts = {
-      menu = {
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                return kind_icon
-              end,
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
-            },
-            kind = {
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
-            },
-          },
-        },
-      },
       keymap = {
         preset = "default",
         -- better selection
