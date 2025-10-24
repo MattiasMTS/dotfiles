8f0e247604027c58878e18ebe09068cd2e7ce975
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-10-24 08:34:29 +0200

    chore: nix flake update

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,8 +1,6 @@
 { username, ... }:
 {
   enable = true;
-  userName = "MattiasMTS";
-  userEmail = "mattias.sjodin.6764@hotmail.com";
 
   signing = {
     key = "/Users/${username}/.ssh/id_ed25519.pub";
@@ -12,7 +10,12 @@
 
   lfs.enable = true;
 
-  extraConfig = {
+  settings = {
+    user = {
+      name = "MattiasMTS";
+      email = "mattias.sjodin.6764@hotmail.com";
+    };
+
     interactive.diffFilter = "delta --color-only";
     delta.navigate = true;
     merge.conflictstyle = "diff3";
@@ -32,35 +35,35 @@
     "includeIf \"gitdir:~/lovablelabs/\"" = {
       email = "msjodin@lovable.dev";
     };
-  };
 
-  aliases = {
-    standup = ''
-      !git log --since yesterday --author="$(git config user.name)\|$(git config user.email)" \
-          --all -i \
-          --format="%C(yellow)%h%Creset|%C(bold blue)%s%Creset|%C(green)%ad%Creset|%C(cyan)%ar%Creset|%C(magenta)%(trailers:key=Resolves,separator=\ )%Creset|-" \
-          --shortstat --date=format:"%Y-%m-%d" | \
-          grep -v "^$" | paste -d"|" - - | \
-          sed -E "s/([0-9]+) files? changed/\x1b[36m\1 files\x1b[0m/g; \
-                  s/([0-9]+) insertions?\(\+\)/\x1b[32m\1+\x1b[0m/g; \
-                  s/([0-9]+) deletions?\(-\)/\x1b[31m\1-\x1b[0m/g" | \
-          column -t -s'|' '';
+    aliases = {
+      standup = ''
+        !git log --since yesterday --author="$(git config user.name)\|$(git config user.email)" \
+            --all -i \
+            --format="%C(yellow)%h%Creset|%C(bold blue)%s%Creset|%C(green)%ad%Creset|%C(cyan)%ar%Creset|%C(magenta)%(trailers:key=Resolves,separator=\ )%Creset|-" \
+            --shortstat --date=format:"%Y-%m-%d" | \
+            grep -v "^$" | paste -d"|" - - | \
+            sed -E "s/([0-9]+) files? changed/\x1b[36m\1 files\x1b[0m/g; \
+                    s/([0-9]+) insertions?\(\+\)/\x1b[32m\1+\x1b[0m/g; \
+                    s/([0-9]+) deletions?\(-\)/\x1b[31m\1-\x1b[0m/g" | \
+            column -t -s'|' '';
 
-    sprint = ''
-      !git log --since="14 days ago" --author="$(git config user.name)\|$(git config user.email)" \
-          --all -i \
-          --format="%C(yellow)%h%Creset|%C(bold blue)%s%Creset|%C(green)%ad%Creset|%C(cyan)%ar%Creset|%C(magenta)%(trailers:key=Resolves,separator=\ )%Creset|-" \
-          --shortstat --date=format:"%Y-%m-%d" | \
-          grep -v "^$" | paste -d"|" - - | \
-          sed -E "s/([0-9]+) files? changed/\x1b[36m\1 files\x1b[0m/g; \
-                  s/([0-9]+) insertions?\(\+\)/\x1b[32m\1+\x1b[0m/g; \
-                  s/([0-9]+) deletions?\(-\)/\x1b[31m\1-\x1b[0m/g" | \
-          column -t -s'|' '';
+      sprint = ''
+        !git log --since="14 days ago" --author="$(git config user.name)\|$(git config user.email)" \
+            --all -i \
+            --format="%C(yellow)%h%Creset|%C(bold blue)%s%Creset|%C(green)%ad%Creset|%C(cyan)%ar%Creset|%C(magenta)%(trailers:key=Resolves,separator=\ )%Creset|-" \
+            --shortstat --date=format:"%Y-%m-%d" | \
+            grep -v "^$" | paste -d"|" - - | \
+            sed -E "s/([0-9]+) files? changed/\x1b[36m\1 files\x1b[0m/g; \
+                    s/([0-9]+) insertions?\(\+\)/\x1b[32m\1+\x1b[0m/g; \
+                    s/([0-9]+) deletions?\(-\)/\x1b[31m\1-\x1b[0m/g" | \
+            column -t -s'|' '';
 
-    hotspots = ''
-      !git log --since '7 days ago' \
-          --author="$(git config user.name)\|$(git config user.email)" \
-          --all -i --pretty=format: --name-only | \
-          sort | uniq -c | sort -rn | head -10'';
+      hotspots = ''
+        !git log --since '7 days ago' \
+            --author="$(git config user.name)\|$(git config user.email)" \
+            --all -i --pretty=format: --name-only | \
+            sort | uniq -c | sort -rn | head -10'';
+    };
   };
 }
