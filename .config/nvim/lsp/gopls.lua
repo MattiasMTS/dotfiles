9aff9c9b658fdce3eafb36d0369c7d997730c180
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-12-04 20:38:40 +0100

    chore: update gopls settings

--- .config/nvim/lsp/gopls.lua
+++ .config/nvim/lsp/gopls.lua
@@ -6,21 +6,35 @@ return {
   -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
   settings = {
     gopls = {
-      buildFlags = { "-tags=wireinject,integration" },
+      codelenses = {
+        gc_details = false,
+        generate = true,
+        regenerate_cgo = true,
+        run_govulncheck = true,
+        test = true,
+        tidy = true,
+        upgrade_dependency = true,
+        vendor = true,
+      },
       analyses = {
         ST1000 = false, -- ignore "docs required in pkg"
+        nilness = true,
+        unusedparams = true,
+        unusedwrite = true,
+        useany = true,
       },
       gofumpt = false, -- replaced with conform
       usePlaceholders = true,
       staticcheck = true,
+      completeUnimported = true,
       vulncheck = "imports",
       hints = {
-        parameterNames = true,
         assignVariableTypes = true,
-        constantValues = true,
-        compositeLiteralTypes = true,
         compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
         functionTypeParameters = true,
+        parameterNames = true,
         rangeVariableTypes = true,
       },
       semanticTokens = true,
