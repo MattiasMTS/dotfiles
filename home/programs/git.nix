56f00a8f4824f50052011cf2c1bf622413491fe5
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-12-04 22:28:08 +0100

    chore: migration aliases -> alias

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -49,34 +49,11 @@
       email = "msjodin@lovable.dev";
     };
 
-    aliases = {
-      standup = ''
-        !git log --since yesterday --author="$(git config user.name)\|$(git config user.email)" \
-            --all -i \
-            --format="%C(yellow)%h%Creset|%C(bold blue)%s%Creset|%C(green)%ad%Creset|%C(cyan)%ar%Creset|%C(magenta)%(trailers:key=Resolves,separator=\ )%Creset|-" \
-            --shortstat --date=format:"%Y-%m-%d" | \
-            grep -v "^$" | paste -d"|" - - | \
-            sed -E "s/([0-9]+) files? changed/\x1b[36m\1 files\x1b[0m/g; \
-                    s/([0-9]+) insertions?\(\+\)/\x1b[32m\1+\x1b[0m/g; \
-                    s/([0-9]+) deletions?\(-\)/\x1b[31m\1-\x1b[0m/g" | \
-            column -t -s'|' '';
-
-      sprint = ''
-        !git log --since="14 days ago" --author="$(git config user.name)\|$(git config user.email)" \
-            --all -i \
-            --format="%C(yellow)%h%Creset|%C(bold blue)%s%Creset|%C(green)%ad%Creset|%C(cyan)%ar%Creset|%C(magenta)%(trailers:key=Resolves,separator=\ )%Creset|-" \
-            --shortstat --date=format:"%Y-%m-%d" | \
-            grep -v "^$" | paste -d"|" - - | \
-            sed -E "s/([0-9]+) files? changed/\x1b[36m\1 files\x1b[0m/g; \
-                    s/([0-9]+) insertions?\(\+\)/\x1b[32m\1+\x1b[0m/g; \
-                    s/([0-9]+) deletions?\(-\)/\x1b[31m\1-\x1b[0m/g" | \
-            column -t -s'|' '';
-
-      hotspots = ''
-        !git log --since '7 days ago' \
-            --author="$(git config user.name)\|$(git config user.email)" \
-            --all -i --pretty=format: --name-only | \
-            sort | uniq -c | sort -rn | head -10'';
+    alias = {
+      standup = "!git log --since=yesterday --author=\"$(git config user.name)\" --all --format='%h %s (%ar)' --shortstat";
+      weekly = "!git log --since='1 week ago' --author=\"$(git config user.name)\" --all --format='%h %s (%ar)' --shortstat";
+      standup-summary = "!git standup | claude -p 'Summarize this git activity for a standup. Be concise, use bullet points.'";
+      weekly-summary = "!git weekly | claude -p 'Summarize this week of git activity. Group by theme/feature. Be concise.'";
     };
   };
 }
